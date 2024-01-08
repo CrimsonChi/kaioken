4713025046bf35c1f5453718b1dca878545b43ec
Robby6Strings <rausten93@gmail.com>
2024-01-09 11:06:07 +1300

    Refactor styleScope.ts to support keyframe selectors

--- package/lib/src/styleScope.ts
+++ package/lib/src/styleScope.ts
@@ -9,6 +9,7 @@ type Props = {
 type StyleRule = {
   selector: string
   body: string
+  isKeyframe?: boolean
 }
 
 let slugOffset = 0
@@ -45,38 +46,68 @@ function StyleScope({ children }: Props) {
 function transformStyles(node: VNode, scopeId: string, rules: StyleRule[]) {
   node.props.children[0].props.nodeValue = rules.reduce((acc, rule) => {
     const selector = rule.selector
-    const body = rule.body.replace(/(\r\n|\n|\r)/gm, "")
-    const scopedSelector = selector
-      .split(",")
-      .map((s) => `${s}.${scopeId}`)
-      .join(",")
-    return `${acc}${scopedSelector}{${body}}`
+    //const body = rule.body.replace(/(\r\n|\n|\r)/gm, "")
+    const scopedSelector = rule.isKeyframe
+      ? selector
+      : selector
+          .split(",")
+          .map((s) => (s ? s : `${s}.${scopeId}`))
+          .join(",")
+    return `${acc}${scopedSelector}{${rule.body}}\n`
   }, "") as string
 }
 
 function applyStyles(node: VNode, rules: StyleRule[], scopeId: string) {
-  const rule = rules.find((r) => r.selector === node.type)
+  const rule = rules.find(
+    (r) =>
+      r.selector === node.type ||
+      (r.selector.startsWith(".") &&
+        r.selector.substring(1) === node.props.className)
+  )
   if (rule) {
-    let className = node.props.className || ""
-    if (rule.selector === node.type) {
-      className += ` ${scopeId}`
-    }
-    node.props.className = className.trim()
+    node.props.className = ((node.props.className || "") + " " + scopeId).trim()
   }
   if (node.props.children) {
     node.props.children.forEach((child) => applyStyles(child, rules, scopeId))
   }
 }
 
+function isKeyframeSelector(selector: string) {
+  return selector.startsWith("@keyframes") || selector.endsWith("%")
+}
+
 function parseCSS(css: string): StyleRule[] {
-  const rules = css.split("}")
+  let openBrackets = 0
+  let selector = ""
+  let body = ""
+  let isKeyframe = false
+  const rules: StyleRule[] = []
+  for (const char of css) {
+    if (char === "{") {
+      if (openBrackets === 0) {
+        selector = selector.trim()
+        isKeyframe = isKeyframeSelector(selector)
+      } else if (isKeyframe) {
+        body += "{"
+      }
+      openBrackets++
+    } else if (char === "}") {
+      openBrackets--
+      if (openBrackets === 0) {
+        body = body.trim()
+        rules.push({ selector, body, isKeyframe })
+        selector = ""
+        body = ""
+      }
+      if (isKeyframe) body += "}"
+    } else {
+      if (openBrackets === 0) {
+        selector += char
+      } else {
+        body += char
+      }
+    }
+  }
+
   return rules
-    .map((r) => {
-      const [selector, body] = r.split("{")
-      if (!selector || !body) return null
-      const trimmedSelector = selector.trim()
-      const trimmedBody = body.trim()
-      return { selector: `${trimmedSelector}`, body: trimmedBody }
-    })
-    .filter((r) => r) as StyleRule[]
 }
