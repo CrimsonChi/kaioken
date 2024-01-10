93d6553d28ed833aa42bf31b2f9f2a752b02b6c8
Robby6Strings <rausten93@gmail.com>
2024-01-10 14:18:15 +1300

    Refactor useOptimistic hook to support queueing of state updates

--- package/lib/src/hooks/useOptimistic.ts
+++ package/lib/src/hooks/useOptimistic.ts
@@ -8,19 +8,27 @@ export function useOptimistic<T, U>(
   const node = getCurrentNode("useState must be called in a component")
   if (!node) return [initial, () => {}]
 
-  const { hook } = getHook(node, {
+  const { hook, oldHook } = getHook(node, {
     state: initial,
     isRenderTrigger: false,
+    queue: [] as Function[],
   })
+
+  if (!oldHook) console.log("new hook")
 
   if (hook.isRenderTrigger) {
     hook.isRenderTrigger = false
   } else {
     hook.state = initial
+    hook.queue.shift()?.(hook.state)
+    for (const f of hook.queue) {
+      hook.state = f(hook.state)
+    }
   }
 
   const setState = (newValue: U) => {
     hook.state = setter(hook.state, newValue)
+    hook.queue.push((state: T) => setter(state, newValue))
     hook.isRenderTrigger = true
     g.setWipNode(node)
   }
