229306ee10fe80f50c24f4534864a429412de70a
Robby6Strings <rausten93@gmail.com>
2024-01-17 06:36:32 +1300

    Add useModel hook for handling form inputs

---
+++ package/lib/src/hooks/useModel.ts
@@ -0,0 +1,69 @@
+import { Ref } from "src/types.js"
+import { useHook } from "./utils.js"
+
+export function useModel<
+  T extends HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement,
+  U extends string | number | boolean
+>(initial: U) {
+  return useHook(
+    "useModel",
+    { value: initial, ref: { current: null } as Ref<T> },
+    ({ hook, oldHook, update, queueEffect }) => {
+      const setValue = (value: U) => {
+        hook.value = value
+        if (hook.ref.current) {
+          setElementValue(hook.ref.current, value)
+        }
+        update()
+      }
+
+      queueEffect(() => {
+        const element = hook.ref.current
+        if (!element) {
+          return
+        }
+        if (!oldHook) setElementValue(element, hook.value)
+
+        const listener = () => {
+          setValue(getElementValue(element) as U)
+        }
+
+        element.addEventListener("input", listener)
+        return () => {
+          element.removeEventListener("input", listener)
+        }
+      })
+
+      return [hook.ref, hook.value, setValue] as const
+    }
+  )
+}
+
+function setElementValue(
+  element: HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement,
+  value: unknown
+) {
+  switch (element.type) {
+    case "checkbox":
+      ;(element as HTMLInputElement).checked = value as boolean
+      break
+    case "radio":
+      ;(element as HTMLInputElement).checked = value === element.value
+      break
+    default:
+      element.value = value as string
+  }
+}
+
+function getElementValue(
+  element: HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+) {
+  switch (element.type) {
+    case "checkbox":
+      return (element as HTMLInputElement).checked
+    case "radio":
+      return (element as HTMLInputElement).value
+    default:
+      return element.value
+  }
+}
