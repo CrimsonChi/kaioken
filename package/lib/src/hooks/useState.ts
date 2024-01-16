fa758defe2360be13a34700389a67d0725ba548c
Robby6Strings <rausten93@gmail.com>
2024-01-17 06:36:41 +1300

    Refactor useState hook to support initial value as a function

--- package/lib/src/hooks/useState.ts
+++ package/lib/src/hooks/useState.ts
@@ -2,12 +2,20 @@ import { useHook } from "./utils.js"
 
 type StateSetter<T> = T | ((prev: T) => T)
 
-export function useState<T>(state: T): [T, (value: StateSetter<T>) => void] {
-  return useHook("useState", { state }, ({ hook, update }) => {
-    const setState = (setter: StateSetter<T>) => {
-      hook.state = setter instanceof Function ? setter(hook.state) : setter
-      update()
+export function useState<T>(
+  initial: T | (() => T)
+): [T, (value: StateSetter<T>) => void] {
+  return useHook(
+    "useState",
+    { state: undefined as T },
+    ({ hook, oldHook, update }) => {
+      if (!oldHook)
+        hook.state = initial instanceof Function ? initial() : initial
+      const setState = (setter: StateSetter<T>) => {
+        hook.state = setter instanceof Function ? setter(hook.state) : setter
+        update()
+      }
+      return [hook.state, setState]
     }
-    return [hook.state, setState]
-  })
+  )
 }
