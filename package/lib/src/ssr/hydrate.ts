7b24679b9e14d3a9888bc0a9f6d63585b4bb0b01
Robby6Strings <rausten93@gmail.com>
2024-01-31 06:14:56 +1300

    hydrate now destructively inits app

--- package/lib/src/ssr/hydrate.ts
+++ package/lib/src/ssr/hydrate.ts
@@ -1,3 +1,6 @@
+import { mount } from "../"
 export function hydrate(appFunc: () => JSX.Element, container: HTMLElement) {
   console.log("hydrate", appFunc, container)
+  container.innerHTML = ""
+  mount(appFunc, container)
 }
