e48a3d9322a166a2732e8a6481c86d59c63e2950
Robby6Strings <rausten93@gmail.com>
2024-02-16 01:13:33 +1300

    Revert "implement initial support of async mutators for createStore"
    This reverts commit 96aa6c0493d0b0f2a4929492011a38d36debad47.

--- package/lib/src/store.ts
+++ package/lib/src/store.ts
@@ -4,7 +4,7 @@ import type { VNode } from "./types"
 
 export { createStore }
 
-type StateSetter<T> = T | ((prev: T) => T | Promise<T>)
+type StateSetter<T> = T | ((prev: T) => T)
 
 function createStore<
   T,
@@ -14,20 +14,14 @@ function createStore<
 >(initial: T, settersFactory: U) {
   let value = initial
   const subscribers = new Set<VNode>()
-  const update = () => subscribers.forEach((n) => ctx.requestUpdate(n))
-  const set = (newVal: T) => ((value = newVal), update())
-
   const mutator = (setter: StateSetter<T>) => {
-    if (!(setter instanceof Function)) {
-      return set(setter)
-    }
-    const tmp = setter(value)
-    return tmp instanceof Promise ? tmp.then(set) : set(tmp)
+    value = setter instanceof Function ? setter(value) : setter
+    subscribers.forEach((n) => ctx.requestUpdate(n))
   }
-
   const factory = settersFactory(mutator) as ReturnType<U>
 
   type S = { value: T } & ReturnType<U>
+
   return function useStore<Selector extends (s: S) => unknown>(fn: Selector) {
     const node = ctx.curNode
     if (node) {
