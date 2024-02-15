96aa6c0493d0b0f2a4929492011a38d36debad47
Robby6Strings <rausten93@gmail.com>
2024-02-16 00:34:54 +1300

    implement initial support of async mutators for createStore

--- package/lib/src/store.ts
+++ package/lib/src/store.ts
@@ -4,7 +4,7 @@ import type { VNode } from "./types"
 
 export { createStore }
 
-type StateSetter<T> = T | ((prev: T) => T)
+type StateSetter<T> = T | ((prev: T) => T | Promise<T>)
 
 function createStore<
   T,
@@ -14,14 +14,20 @@ function createStore<
 >(initial: T, settersFactory: U) {
   let value = initial
   const subscribers = new Set<VNode>()
+  const update = () => subscribers.forEach((n) => ctx.requestUpdate(n))
+  const set = (newVal: T) => ((value = newVal), update())
+
   const mutator = (setter: StateSetter<T>) => {
-    value = setter instanceof Function ? setter(value) : setter
-    subscribers.forEach((n) => ctx.requestUpdate(n))
+    if (!(setter instanceof Function)) {
+      return set(setter)
+    }
+    const tmp = setter(value)
+    return tmp instanceof Promise ? tmp.then(set) : set(tmp)
   }
+
   const factory = settersFactory(mutator) as ReturnType<U>
 
   type S = { value: T } & ReturnType<U>
-
   return function useStore<Selector extends (s: S) => unknown>(fn: Selector) {
     const node = ctx.curNode
     if (node) {
--- sandbox/demo/src/store.ts
+++ sandbox/demo/src/store.ts
@@ -1,7 +1,11 @@
 import { createStore } from "kaioken"
 
 export const useCountStore = createStore({ count: 0 }, (set) => ({
-  increment: () => set((state) => ({ count: state.count + 1 })),
+  increment: () =>
+    set(async (state) => {
+      await new Promise((res) => setTimeout(res, 500))
+      return { count: state.count + 1 }
+    }),
   decrement: () => set((state) => ({ count: state.count - 1 })),
   add: (value: number) => set((state) => ({ count: state.count + value })),
   reset: () => set({ count: 0 }),
