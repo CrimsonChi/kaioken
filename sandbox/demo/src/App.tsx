3128ddfb71b662bcabef1aef2c31b197d8ec5f3b
Robby6Strings <rausten93@gmail.com>
2024-02-21 03:07:45 +1300

    ^version

--- sandbox/demo/src/App.tsx
+++ sandbox/demo/src/App.tsx
@@ -1,4 +1,4 @@
-import { Router, Route } from "kaioken"
+import { Router, Route, useEffect } from "kaioken"
 import { Todos } from "./components/ToDos"
 import { Counter } from "./components/Counter"
 import { ThemeContextProvider } from "./ThemeContext"
@@ -9,11 +9,28 @@ import { DrawerDemo } from "./components/dialog/Drawer"
 import { H1 } from "./components/atoms/Heading"
 import { Link } from "./components/atoms/Link"
 
+let start = performance.now()
+
+function Home() {
+  const length = 10e3
+  useEffect(() => {
+    console.log(`effect took ${performance.now() - start}ms`)
+  }, [])
+  const children = Array.from({ length }).map((_, idx) => <li>{idx}</li>)
+  return (
+    <div>
+      <H1>Home</H1>
+      <div>
+        <ul>{children}</ul>
+      </div>
+    </div>
+  )
+}
+
 export function App() {
   return (
     <ThemeContextProvider>
       <nav className="flex flex-col flex-wrap gap-2 min-h-screen p-2 justify-center mb-5">
-        <H1 className="mb-5">App</H1>
         <Link to="/">Home</Link>
         <Link to="/todos">Todos (state, model, memo)</Link>
         <Link to="/counter">Counter (store)</Link>
@@ -23,7 +40,7 @@ export function App() {
       </nav>
       <main className="flex items-center justify-center flex-grow w-full">
         <Router>
-          <Route path="/" element={() => <h2>Home</h2>} />
+          <Route path="/" element={Home} />
           <Route path="/todos" element={Todos} />
           <Route path="/counter" element={Counter} />
           <Route path="/query" element={ProductPage} />
