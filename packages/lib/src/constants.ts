7c48068257a4958e8d0a23d76585be092e830ba9
Robby6Strings <rausten93@gmail.com>
2024-02-29 19:55:49 +1300

    EffectTag -> no longer an enum

--- packages/lib/src/constants.ts
+++ packages/lib/src/constants.ts
@@ -1,8 +1,8 @@
-export enum EffectTag {
-  UPDATE = 1,
-  PLACEMENT = 2,
-  DELETION = 3,
-}
+export const EffectTag = {
+  UPDATE: 1,
+  PLACEMENT: 2,
+  DELETION: 3,
+} as const
 
 export const componentSymbol = Symbol.for("kaioken.component")
 export const elementFreezeSymbol = Symbol.for("kaioken.freezeElement")
