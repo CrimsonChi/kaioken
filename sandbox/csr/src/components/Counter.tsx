875c1956f0db056d65e283eac64c66b8e1695611
Robby6Strings <rausten93@gmail.com>
2024-04-13 04:20:40 +1200

    sandbox/csr - remove random useAsync call

--- sandbox/csr/src/components/Counter.tsx
+++ sandbox/csr/src/components/Counter.tsx
@@ -1,25 +1,12 @@
 import { Button } from "./atoms/Button"
 import { Container } from "./atoms/Container"
 import { useCountStore } from "../store"
-import { useAsync } from "kaioken"
 
 export function Counter() {
   const { value: count, increment, decrement } = useCountStore()
 
-  const data = useAsync(
-    () =>
-      fetch(`https://dummyjson.com/products/${count + 1}`).then((res) => {
-        console.log("resolving req")
-        return res.json()
-      }),
-    [count]
-  )
-
-  console.log("Counter", data)
-
   return (
     <div className="flex flex-col">
-      <Container>{data?.title}</Container>
       <Container className="flex gap-2 items-center">
         <Button onclick={decrement}>-1</Button>
         <span>count: {count}</span>
