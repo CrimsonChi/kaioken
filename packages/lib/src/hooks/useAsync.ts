c507a01f19af58b788a37a85fe8f0e0c3d9112c7
Robby6Strings <rausten93@gmail.com>
2024-04-13 03:38:56 +1200

    update useAsync to better fill the void left by removal of useFetch

--- packages/lib/src/hooks/useAsync.ts
+++ packages/lib/src/hooks/useAsync.ts
@@ -1,27 +1,50 @@
 import { depsRequireChange, shouldExecHook, useHook } from "./utils.js"
 
-export function useAsync<T>(func: () => Promise<T>, deps: unknown[]): T | null {
-  if (!shouldExecHook()) return null
+type UseAsyncResult<T> =
+  | [T, false, null] // loaded
+  | [null, true, null] // loading
+  | [null, false, any] // error
+
+export function useAsync<T>(
+  func: () => Promise<T>,
+  deps: unknown[]
+): UseAsyncResult<T> {
+  if (!shouldExecHook()) return [null, true, null] as const
   return useHook(
     "useAsync",
     {
       deps,
       data: null as T | null,
+      error: null as any | null,
+      loading: true as boolean,
       promise: undefined as Promise<T> | undefined,
     },
     ({ hook, oldHook, update }) => {
       if (depsRequireChange(deps, oldHook?.deps)) {
+        hook.data = null
+        hook.loading = true
+        hook.error = null
         hook.deps = deps
         hook.promise = func()
-        hook.data = null
-        hook.promise.then((data: T) => {
-          if (!depsRequireChange(deps, hook.deps)) {
-            hook.data = data
-            update()
-          }
-        })
+        hook.promise
+          .then((data: T) => {
+            if (!depsRequireChange(deps, hook.deps)) {
+              hook.data = data
+              hook.loading = false
+              hook.error = null
+              update()
+            }
+          })
+          .catch((error) => {
+            if (!depsRequireChange(deps, hook.deps)) {
+              hook.data = null
+              hook.loading = false
+              hook.error = error
+              update()
+            }
+          })
       }
-      return hook.data
+      return [hook.data, hook.loading, hook.error] as UseAsyncResult<T>
     }
   )
 }
