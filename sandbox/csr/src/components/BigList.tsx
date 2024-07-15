a3e45860bdc7171a4bdbadfebdfbbeab0a3e0067
Robby6Strings <rausten93@gmail.com>
2024-07-15 20:13:03 +1200

    sandbox/csr large list rendering - add perf stamps

--- sandbox/csr/src/components/BigList.tsx
+++ sandbox/csr/src/components/BigList.tsx
@@ -1,11 +1,38 @@
+import { memo, useEffect, useState } from "kaioken"
+
 export function BigListComponent() {
+  const [toggle, setToggle] = useState(false)
+
+  let startTime = performance.now()
+  useEffect(() => {
+    console.log("render big list", performance.now() - startTime)
+  })
+
+  const handleClick = () => {
+    setToggle(!toggle)
+    startTime = performance.now()
+  }
+
   return (
     <div>
+      <button className="fixed top-0 right-0" onclick={handleClick}>
+        Toggle
+      </button>
       <ul>
-        {Array.from({ length: 100e3 }).map((_, i) => (
-          <li>{i}</li>
-        ))}
+        <Items />
+        {toggle && <article>One More!</article>}
       </ul>
     </div>
   )
 }
+
+const Items = memo(() => {
+  console.log("render items")
+  return (
+    <>
+      {Array.from({ length: 100e3 }).map((_, i) => (
+        <li>{i}</li>
+      ))}
+    </>
+  )
+})
