2dac2f7255fa8831d1e80a6ca561fb93c589ef6f
Robby6Strings <rausten93@gmail.com>
2024-07-16 19:33:59 +1200

    create-kaioken - replace 'download-git-repo' dependancy, include license file

--- packages/create-kaioken/index.js
+++ packages/create-kaioken/index.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-
-import download from "download-git-repo"
+import fs from "node:fs"
+import { simpleGit } from "simple-git"
 import { program } from "commander"
 import inquirer from "inquirer"
 
 const templates = [
   {
     name: "CSR (Client-side rendering)",
-    value: "CrimsonChi/kaioken-csr-template",
+    value: "https://github.com/CrimsonChi/kaioken-csr-template.git",
   },
   {
     name: "SSR/SSG (Server-side rendering)",
-    value: "CrimsonChi/kaioken-ssr-template",
+    value: "https://github.com/CrimsonChi/kaioken-ssr-template.git",
   },
 ]
 
@@ -50,16 +50,46 @@ program
       dest = selectedDest
     }
 
+    // check if the folder exists already and ask if it should be overwritten
+    if (fs.existsSync(dest)) {
+      const { overwrite } = await inquirer.prompt([
+        {
+          type: "list",
+          name: "overwrite",
+          message: "The folder already exists. Do you want to overwrite it?",
+          choices: [
+            {
+              name: "Yes",
+              value: "y",
+            },
+            {
+              name: "No",
+              value: "n",
+            },
+          ],
+        },
+      ])
+      if (overwrite !== "y") return
+      fs.rmSync(dest, { recursive: true, force: true })
+    }
+
     console.log(`Downloading project template '${template}'...`)
-    await new Promise((resolve, reject) => {
-      download(template, dest, (err) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve()
-        }
-      })
-    })
+    try {
+      const git = simpleGit()
+      await git.clone(template, dest)
+    } catch (error) {
+      console.error(
+        `[create-kaioken]: An error occurred while cloning the template:`,
+        error
+      )
+      return
+    }
+    // remove '.git' folder
+
+    const gitFolder = `${dest}/.git`
+    if (fs.existsSync(gitFolder)) {
+      fs.rmSync(gitFolder, { recursive: true, force: true })
+    }
     console.log(`Project template downloaded. Get started by running the following:
 
   cd ${dest}
