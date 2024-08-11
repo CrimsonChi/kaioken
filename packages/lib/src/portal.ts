76308bd45e5b6e4601699fe968ec0ef391a2bad9
Robby6Strings <rausten93@gmail.com>
2024-08-12 01:34:41 +1200

    hydration - no longer attempt to hydrate children of Portal as they are assumed to never be rendered by SSR. children of Portal that are visible by default are now rendered post-hydration.

--- packages/lib/src/portal.ts
+++ packages/lib/src/portal.ts
@@ -1,4 +1,7 @@
+import type { AppContext } from "./appContext.js"
 import { Component } from "./component.js"
+import { renderMode } from "./globals.js"
+import { useAppContext } from "./hooks/utils.js"
 
 export { Portal }
 
@@ -12,9 +15,12 @@ const portalIdentifier = Symbol.for("kaioken.portal")
 class Portal extends Component<PortalProps> {
   doNotModifyDom = true
   static [portalIdentifier] = true
+  ctx: AppContext
+
   constructor(props: PortalProps) {
     super(props)
     this.vNode.dom = this.props.container
+    this.ctx = useAppContext(this.vNode)
   }
 
   static isPortal(type: unknown): type is typeof Portal {
@@ -22,6 +28,12 @@ class Portal extends Component<PortalProps> {
   }
 
   render(): JSX.Element {
+    if (renderMode.current === "hydrate") {
+      this.ctx.scheduler?.nextIdle(() => {
+        this.ctx.requestUpdate(this.vNode)
+      })
+      return null
+    }
     return this.props.children ?? null
   }
 }
