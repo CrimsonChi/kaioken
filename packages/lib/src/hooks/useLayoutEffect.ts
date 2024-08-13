9fc1604cef849b60730839e4f1afdf395be05f61
Yofou <reach@yofou.dev>
2024-08-13 15:16:41 +0100

    chore: added useLayoutEffect

---
+++ packages/lib/src/hooks/useLayoutEffect.ts
@@ -0,0 +1,26 @@
+import {
+  cleanupHook,
+  depsRequireChange,
+  sideEffectsEnabled,
+  useHook,
+} from "./utils.js"
+
+export function useEffect(
+  callback: () => void | (() => void),
+  deps?: unknown[]
+): void {
+  if (!sideEffectsEnabled()) return
+  return useHook("useEffect", { deps }, ({ hook, oldHook }) => {
+    if (depsRequireChange(deps, oldHook?.deps)) {
+      hook.deps = deps
+      if (oldHook) {
+        cleanupHook(oldHook)
+      }
+
+			const cleanup = callback()
+			if (cleanup && typeof cleanup === "function") {
+					hook.cleanup = cleanup
+			}
+    }
+  })
+}
