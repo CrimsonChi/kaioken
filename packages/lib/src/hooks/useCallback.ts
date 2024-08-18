f148b2849840681cc61ab2f0cd464aad1cdecb2e
Robby6Strings <rausten93@gmail.com>
2024-08-19 03:30:36 +1200

    lib/hooks/useCallback - remove unneccesary optional chain

--- packages/lib/src/hooks/useCallback.ts
+++ packages/lib/src/hooks/useCallback.ts
@@ -7,7 +7,7 @@ export function useCallback<T extends Function>(
   if (!sideEffectsEnabled()) return callback
 
   return useHook("useCallback", { callback, deps }, ({ hook, isInit }) => {
-    if (isInit || depsRequireChange(deps, hook?.deps)) {
+    if (isInit || depsRequireChange(deps, hook.deps)) {
       hook.deps = deps
       hook.callback = callback
     }
