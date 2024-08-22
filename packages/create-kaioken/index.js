55b9b1d83da598737810bde8886afac2cf798a95
Robby6Strings <rausten93@gmail.com>
2024-08-23 02:16:59 +1200

    create-kaioken - make project destination the first arg, improve prompt messages, check for/handle file-as-dest case, improve dest-not-empty handling

--- packages/create-kaioken/index.js
+++ packages/create-kaioken/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import fs from "node:fs"
+import path from "node:path"
 import { simpleGit } from "simple-git"
 import { program } from "commander"
 import inquirer from "inquirer"
@@ -15,20 +16,91 @@ const templates = [
   },
 ]
 
-const defaultDir = "my-new-app"
+const defaultDir = "."
 
 program
+  .option("-d, --dest <dest>", "Destination directory")
   .option("-csr, --csr", "Use CSR template")
   .option("-ssr, --ssr", "Use SSR template")
-  .option("-d, --dest <dest>", "Destination directory")
   .action(async ({ dest, csr, ssr }) => {
+    console.log("Welcome to Kaioken!\n")
+    if (!dest) {
+      const { selectedDest } = await inquirer.prompt([
+        {
+          type: "input",
+          name: "selectedDest",
+          message:
+            "Where should we create your project? \n(default: current directory)",
+          default: defaultDir,
+        },
+      ])
+      dest = selectedDest
+    }
+
+    // if the folder exists already and contains files, ask if it should be overwritten
+    if (fs.existsSync(dest)) {
+      const stats = fs.statSync(dest)
+      if (stats.isDirectory()) {
+        const destChildren = fs.readdirSync(dest)
+        if (destChildren.length > 0) {
+          const { overwrite } = await inquirer.prompt([
+            {
+              type: "list",
+              name: "overwrite",
+              message:
+                "The folder already exists and is not empty. \nIn order to proceed, it will be deleted. Do you want to continue?",
+              choices: [
+                {
+                  name: "Yes",
+                  value: "y",
+                },
+                {
+                  name: "No",
+                  value: "n",
+                },
+              ],
+            },
+          ])
+          if (overwrite === "n") return
+
+          for (const file of destChildren) {
+            const filePath = path.join(dest, file)
+            fs.rmSync(filePath, { recursive: true, force: true })
+          }
+        }
+      } else if (stats.isFile()) {
+        const { overwrite } = await inquirer.prompt([
+          {
+            type: "list",
+            name: "overwrite",
+            message:
+              "There is already a file at the destination. \nIn order to proceed, it will be deleted. Do you want to continue?",
+            choices: [
+              {
+                name: "Yes",
+                value: "y",
+              },
+              {
+                name: "No",
+                value: "n",
+              },
+            ],
+          },
+        ])
+        if (overwrite === "n") return
+        fs.rmSync(dest, { recursive: true, force: true })
+      } else {
+        throw new Error(`Unknown file type at ${dest}. Exiting...`)
+      }
+    }
+
     let template = ""
     if ((csr && ssr) || (!csr && !ssr)) {
       const { selectedTemplate } = await inquirer.prompt([
         {
           type: "list",
           name: "selectedTemplate",
-          message: "Choose a template:",
+          message: "Which template do you want to use?",
           choices: templates,
         },
       ])
@@ -36,41 +108,6 @@ program
     } else {
       if (csr) template = templates[0].value
       if (ssr) template = templates[1].value
-    }
-
-    if (!dest) {
-      const { selectedDest } = await inquirer.prompt([
-        {
-          type: "input",
-          name: "selectedDest",
-          message: "Destination directory:",
-          default: defaultDir,
-        },
-      ])
-      dest = selectedDest
-    }
-
-    // check if the folder exists already and ask if it should be overwritten
-    if (fs.existsSync(dest)) {
-      const { overwrite } = await inquirer.prompt([
-        {
-          type: "list",
-          name: "overwrite",
-          message: "The folder already exists. Do you want to overwrite it?",
-          choices: [
-            {
-              name: "Yes",
-              value: "y",
-            },
-            {
-              name: "No",
-              value: "n",
-            },
-          ],
-        },
-      ])
-      if (overwrite !== "y") return
-      fs.rmSync(dest, { recursive: true, force: true })
     }
 
     console.log(`Downloading project template '${template}'...`)
@@ -84,8 +121,8 @@ program
       )
       return
     }
-    // remove '.git' folder
 
+    // remove '.git' folder
     const gitFolder = `${dest}/.git`
     if (fs.existsSync(gitFolder)) {
       fs.rmSync(gitFolder, { recursive: true, force: true })
