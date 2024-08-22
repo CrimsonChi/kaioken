34092f565785a4515c1b658412329df2f80fa12d
Robby6Strings <rausten93@gmail.com>
2024-08-22 21:18:45 +1200

    selectedNode view - add refresh button

--- packages/devtools-client/src/components/SelectedNodeView.tsx
+++ packages/devtools-client/src/components/SelectedNodeView.tsx
@@ -2,6 +2,7 @@ import { AppContext, useEffect, useRequestUpdate } from "kaioken"
 import { useDevtoolsStore, kaiokenGlobal } from "../store"
 import { getNodeName } from "../utils"
 import { NodeDataSection } from "./NodeDataSection"
+import { RefreshIcon } from "./RefreshIcon"
 
 export function SelectedNodeView() {
   const {
@@ -27,14 +28,22 @@ export function SelectedNodeView() {
     return () => kaiokenGlobal?.off("update", handleUpdate)
   }, [])
 
+  const refresh = () => {
+    if (!selectedNode || !selectedApp) return
+    selectedApp.requestUpdate(selectedNode)
+  }
+
   if (selectedNode === null) return null
   const props = { ...selectedNode.props } as Record<string, any>
   delete props.children
 
   return (
     <div className="flex-grow p-2 sticky top-0">
-      <h2 className="font-bold mb-2 pb-2 border-b-2 border-neutral-800">
+      <h2 className="flex justify-between items-center font-bold mb-2 pb-2 border-b-2 border-neutral-800">
         {"<" + getNodeName(selectedNode) + ">"}
+        <button onclick={refresh}>
+          <RefreshIcon className="w-5 h-5 opacity-50 hover:opacity-100 transition-opacity" />
+        </button>
       </h2>
       <NodeDataSection title="props">
         <pre className="p-2 bg-neutral-800">
