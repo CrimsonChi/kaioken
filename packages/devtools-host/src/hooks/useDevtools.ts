5894fc77719837af4c9f789bd2a52c2ea34c860e
Robby6Strings <rausten93@gmail.com>
2024-08-25 12:59:55 +1200

    devtooks - make popup initialize at more sane default size, persist size via sessionStorage

--- packages/devtools-host/src/hooks/useDevtools.ts
+++ packages/devtools-host/src/hooks/useDevtools.ts
@@ -1,13 +1,27 @@
 import { useCallback } from "kaioken"
 import { popup } from "../store"
 
+type SavedSize = {
+  width: number
+  height: number
+}
+
+const SIZE_STORAGE_KEY = "kaioken-devtools-popup-size"
+
 export const useDevTools = () => {
   const _popup = popup.value
 
   const handleOpen = useCallback(
     (onOpened?: (window: Window) => void) => {
       if (_popup) return _popup.focus()
-      const features = `popup,width=${Math.floor(window.screen.width / 2)},height=${Math.floor(window.screen.height / 2)};`
+      const savedSize_raw = sessionStorage.getItem(SIZE_STORAGE_KEY)
+      const size = savedSize_raw
+        ? (JSON.parse(savedSize_raw) as SavedSize)
+        : {
+            width: Math.floor(window.innerWidth / 2),
+            height: Math.floor(window.innerHeight / 2),
+          }
+      const features = `popup,width=${size.width},height=${size.height};`
       const w = window.open("/__devtools__", "_blank", features)
       if (!w) return console.error("[kaioken]: Unable to open devtools window")
 
@@ -19,6 +33,15 @@ export const useDevTools = () => {
           console.debug("[kaioken]: devtools window closed")
           popup.value = null
         }
+      }
+      w.onresize = () => {
+        sessionStorage.setItem(
+          SIZE_STORAGE_KEY,
+          JSON.stringify({
+            width: w.innerWidth,
+            height: w.innerHeight,
+          })
+        )
       }
     },
     [_popup]
