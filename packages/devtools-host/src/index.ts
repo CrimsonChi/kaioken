89025a96d9e2380ba5de0ee884c9f02ddbb86328
Robby6Strings <rausten93@gmail.com>
2024-08-26 18:49:52 +1200

    devtools-host - improve mounting logic

--- packages/devtools-host/src/index.ts
+++ packages/devtools-host/src/index.ts
@@ -3,37 +3,33 @@ import App from "./App"
 import tailwindCssKaiokenDevToolCssInline from "inline:./style.css"
 import { popup } from "./store"
 ;(() => {
-  if ("window" in globalThis) {
-    //@ts-ignore
-    if (window.__kaiokenDevtools) return
+  if (!("window" in globalThis)) return
+  let hasMounted = false
+  window.__kaioken?.on("mount", async () => {
+    if (hasMounted) return
+    hasMounted = true
 
-    window.__kaioken?.on("mount", () => {
-      //@ts-ignore
-      if (window.__kaiokenDevtools) return
+    const dummy = document.createElement("div")
+    dummy.setAttribute("style", "display: contents")
+    document.body.appendChild(dummy)
 
-      const dummy = document.createElement("div")
-      dummy.setAttribute("style", "display: contents")
-      document.body.appendChild(dummy)
+    const shadow = dummy.attachShadow({ mode: "open" })
+    const sheet = new CSSStyleSheet()
+    sheet.replaceSync(tailwindCssKaiokenDevToolCssInline)
+    shadow.adoptedStyleSheets = [sheet]
 
-      const shadow = dummy.attachShadow({ mode: "open" })
-      const sheet = new CSSStyleSheet()
-      sheet.replaceSync(tailwindCssKaiokenDevToolCssInline)
-      shadow.adoptedStyleSheets = [sheet]
-
-      const root = Object.assign(document.createElement("div"), {
-        id: "devtools-root",
-      })
-      root.setAttribute("class", "fixed flex bottom-0 right-0 z-[9999999]")
-      shadow.appendChild(root)
-
-      //@ts-ignore
-      window.__kaiokenDevtools = mount(App, {
-        root,
-        name: "kaioken.devtools",
-      })
-      window.addEventListener("beforeunload", () => {
-        popup.value?.close()
-      })
+    const root = Object.assign(document.createElement("div"), {
+      id: "devtools-root",
     })
-  }
+    root.setAttribute("class", "fixed flex bottom-0 right-0 z-[9999999]")
+    shadow.appendChild(root)
+
+    mount(App, {
+      root,
+      name: "kaioken.devtools",
+    })
+    const handleMainWindowClose = () => popup.value?.close()
+    window.addEventListener("close", handleMainWindowClose)
+    window.addEventListener("beforeunload", handleMainWindowClose)
+  })
 })()
