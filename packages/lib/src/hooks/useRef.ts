ca25a8cc35a4a9746a9231ce6a06fe24b29b2b85
LankyMoose <rausten93@gmail.com>
2025-04-03 03:53:13 +1300

    lib/useRef - store ref under sub-property to avoid polluting final value

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -2,7 +2,7 @@ import { __DEV__ } from "../env.js"
 import { HookCallbackState, sideEffectsEnabled, useHook } from "./utils.js"
 
 type UseCallbackState<T> = {
-  current: T
+  ref: { current: T }
 }
 
 export function useRef<T>(initialValue: T): Kaioken.MutableRefObject<T>
@@ -10,7 +10,7 @@ export function useRef<T>(initialValue: T | null): Kaioken.RefObject<T>
 export function useRef<T = undefined>(): Kaioken.MutableRefObject<T | undefined>
 export function useRef<T>(initialValue?: T | null) {
   if (!sideEffectsEnabled()) return { current: initialValue }
-  return useHook("useRef", { current: initialValue }, useRefCallback)
+  return useHook("useRef", { ref: { current: initialValue } }, useRefCallback)
 }
 
 const useRefCallback = <T>({
@@ -20,10 +20,10 @@ const useRefCallback = <T>({
   if (isInit) {
     if (__DEV__) {
       hook.debug = {
-        get: () => ({ value: hook.current }),
-        set: ({ value }) => (hook.current = value),
+        get: () => ({ value: hook.ref.current }),
+        set: ({ value }) => (hook.ref.current = value),
       } satisfies Kaioken.HookDebug<{ value: T }>
     }
   }
-  return hook
+  return hook.ref
 }
