0971fe5f39dfa7847b7ee7bb026d18e2aadc97d3
LankyMoose <rausten93@gmail.com>
2025-04-03 21:37:45 +1300

    lib/hooks - impl 'useEffectEvent'

---
+++ packages/lib/src/hooks/useEffectEvent.ts
@@ -0,0 +1,18 @@
+import { node } from "../globals.js"
+import { __DEV__ } from "../env.js"
+import { sideEffectsEnabled, useHook } from "./utils.js"
+
+export function useEffectEvent<T extends Function>(callback: T): T {
+  if (!sideEffectsEnabled()) return callback
+  return useHook("useEffectEvent", { callback }, ({ hook }) => {
+    hook.callback = callback
+    return function () {
+      if (node.current) {
+        throw new Error(
+          "A function wrapped in useEffectEvent can't be called during rendering."
+        )
+      }
+      return hook.callback.apply(void 0, arguments)
+    } as any as T
+  })
+}
