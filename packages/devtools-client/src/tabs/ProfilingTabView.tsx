3088cd86ab2e84d3a980c7bff051b7bc16808afd
LankyMoose <rausten93@gmail.com>
2025-05-05 04:38:35 +1200

    devtools - display granular app events via chart.js timeseries graph

--- packages/devtools-client/src/tabs/ProfilingTabView.tsx
+++ packages/devtools-client/src/tabs/ProfilingTabView.tsx
@@ -1,6 +1,17 @@
-import { AppContext, useEffect, useRequestUpdate } from "kaioken"
+import {
+  AppContext,
+  useEffect,
+  useRef,
+  useRequestUpdate,
+  useSignal,
+} from "kaioken"
 import { kaiokenGlobal } from "../store"
-import { isDevtoolsApp, typedMapEntries } from "devtools-shared"
+import {
+  isDevtoolsApp,
+  LineChart,
+  LineChartData,
+  typedMapEntries,
+} from "devtools-shared"
 
 export function ProfilingTabView() {
   const requestUpdate = useRequestUpdate()
@@ -29,11 +40,84 @@ export function ProfilingTabView() {
   )
 }
 
+const MAX_TICKS = 100
+
+const createLineChartDatasets = (
+  updates: number[],
+  updateDirtied: number[],
+  createNode: number[],
+  updateNode: number[],
+  removeNode: number[]
+): LineChartData["datasets"] => {
+  return [
+    {
+      label: "update",
+      data: updates,
+      fill: false,
+      borderColor: "#ad981f",
+      tension: 0.1,
+      pointStyle: false,
+    },
+    {
+      label: "updateDirtied",
+      data: updateDirtied,
+      fill: false,
+      borderColor: "#b21f3a",
+      tension: 0.1,
+      pointStyle: false,
+    },
+    {
+      label: "createNode",
+      data: createNode,
+      fill: false,
+      borderColor: "#198019",
+      tension: 0.1,
+      pointStyle: false,
+    },
+    {
+      label: "removeNode",
+      data: removeNode,
+      fill: false,
+      borderColor: "#5F3691",
+      tension: 0.1,
+      pointStyle: false,
+    },
+    {
+      label: "updateNode",
+      data: updateNode,
+      fill: false,
+      borderColor: "#2f2f9d",
+      tension: 0.1,
+      pointStyle: false,
+    },
+  ]
+}
+
+const initialLineChartDatasets = createLineChartDatasets(
+  [0],
+  [0],
+  [0],
+  [0],
+  [0]
+)
+
 type AppProfilingViewProps = {
   app: AppContext
 }
 function AppProfilingView({ app }: AppProfilingViewProps) {
   const requestUpdate = useRequestUpdate()
+  const updateEvents = useRef<number[]>([0])
+  const updateDirtiedEvents = useRef<number[]>([0])
+  const createNodeEvents = useRef<number[]>([0])
+  const removeNodeEvents = useRef<number[]>([0])
+  const diffNodeEvents = useRef<number[]>([0])
+  const lineChartData = useSignal<LineChartData>({
+    labels: [(performance.now() / 1000).toFixed(2)],
+    datasets: initialLineChartDatasets,
+  })
+  const chartHovered = useSignal(false)
+  const profilingContext = kaiokenGlobal?.profilingContext!
+
   useEffect(() => {
     const onUpdate = (_app: AppContext) => {
       if (_app.id !== app.id) return
@@ -42,14 +126,96 @@ function AppProfilingView({ app }: AppProfilingViewProps) {
     kaiokenGlobal?.on("update", onUpdate)
     return () => kaiokenGlobal?.off("update", onUpdate)
   }, [])
-  const profilingContext = kaiokenGlobal?.profilingContext!
+
+  useEffect(() => {
+    const onUpdate = (_app: AppContext) => {
+      if (_app.id !== app.id) return
+      const arr = updateEvents.current
+      arr[arr.length - 1]++
+    }
+    const onUpdateDirtied = (_app: AppContext) => {
+      if (_app.id !== app.id) return
+      const arr = updateDirtiedEvents.current
+      arr[arr.length - 1]++
+    }
+    const onCreateNode = (_app: AppContext) => {
+      if (_app.id !== app.id) return
+      const arr = createNodeEvents.current
+      arr[arr.length - 1]++
+    }
+    const onUpdateNode = (_app: AppContext) => {
+      if (_app.id !== app.id) return
+      const arr = diffNodeEvents.current
+      arr[arr.length - 1]++
+    }
+    const onRemoveNode = (_app: AppContext) => {
+      if (_app.id !== app.id) return
+      const arr = removeNodeEvents.current
+      arr[arr.length - 1]++
+    }
+    profilingContext.addEventListener("update", onUpdate)
+    profilingContext.addEventListener("updateDirtied", onUpdateDirtied)
+    profilingContext.addEventListener("createNode", onCreateNode)
+    profilingContext.addEventListener("updateNode", onUpdateNode)
+    profilingContext.addEventListener("removeNode", onRemoveNode)
+
+    const updateInterval = setInterval(() => {
+      const newLabels = [...lineChartData.value.labels]
+      ;[
+        updateEvents.current,
+        updateDirtiedEvents.current,
+        createNodeEvents.current,
+        diffNodeEvents.current,
+        removeNodeEvents.current,
+      ].forEach((arr) => {
+        arr.push(0)
+        if (arr.length > MAX_TICKS) {
+          arr.shift()
+        }
+      })
+      newLabels.push((performance.now() / 1000).toFixed(2))
+      if (newLabels.length > MAX_TICKS) {
+        newLabels.shift()
+      }
+      if (chartHovered.peek() === true) return
+      lineChartData.value = {
+        labels: newLabels,
+        datasets: createLineChartDatasets(
+          updateEvents.current,
+          updateDirtiedEvents.current,
+          createNodeEvents.current,
+          diffNodeEvents.current,
+          removeNodeEvents.current
+        ),
+      }
+    }, 100)
+
+    return () => {
+      profilingContext.removeEventListener("update", onUpdate)
+      profilingContext.removeEventListener("updateDirtied", onUpdateDirtied)
+      profilingContext.removeEventListener("createNode", onCreateNode)
+      profilingContext.removeEventListener("updateNode", onUpdateNode)
+      clearInterval(updateInterval)
+    }
+  }, [])
 
   return (
     <div className="flex flex-col gap-2 border border-white border-opacity-10 rounded bg-neutral-400 bg-opacity-5 text-neutral-400 p-2">
-      <div className="flex gap-2 justify-between">
-        <span>{app.name}</span>
+      <div
+        className="grid items-start"
+        style="grid-template-columns: 1fr max-content;"
+      >
+        <div>
+          <span>{app.name}</span>
+          <LineChart
+            data={lineChartData}
+            className="w-full max-w-full min-h-20"
+            onmouseenter={() => (chartHovered.value = true)}
+            onmouseleave={() => (chartHovered.value = false)}
+          />
+        </div>
         <div
-          className="text-sm grid grid-cols-2 gap-x-4"
+          className="text-xs grid grid-cols-2 gap-x-4"
           style="grid-template-columns: auto auto;"
         >
           <span className="text-right">Mount duration:</span>
---
+++ packages/devtools-shared/src/LineChart.tsx
@@ -0,0 +1,83 @@
+import Chart, { type PointStyle } from "chart.js/auto"
+import zoomPlugin from "chartjs-plugin-zoom"
+
+import { type ElementProps, type Signal, useEffect, useRef } from "kaioken"
+
+export type LineChartData = {
+  labels: string[]
+  datasets: {
+    label: string
+    data: number[]
+    fill: boolean
+    borderColor: string
+    tension: number
+    pointStyle?: PointStyle
+  }[]
+}
+
+export type LineChartProps = Omit<ElementProps<"canvas">, "ref"> & {
+  data: Signal<LineChartData>
+}
+
+export function LineChart({ data, ...props }: LineChartProps) {
+  const chartInstance = useRef<Chart | null>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  useEffect(() => {
+    Chart.register(zoomPlugin)
+    const canvas = canvasRef.current!
+    const chart = (chartInstance.current = new Chart(canvas, {
+      type: "line",
+      data: data.peek(),
+      options: {
+        scales: {
+          y: {
+            min: 0,
+          },
+        },
+        animation: false,
+        responsive: true,
+        plugins: {
+          zoom: {
+            pan: {
+              enabled: true,
+              mode: "x",
+            },
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              mode: "x",
+            },
+          },
+          legend: {
+            position: "top",
+          },
+          title: {
+            display: false,
+          },
+        },
+      },
+    }))
+
+    const unsub = data.subscribe((newData) => {
+      for (const dataset of newData.datasets) {
+        const existing = chart.data.datasets.find(
+          (d) => d.label === dataset.label
+        )
+        if (existing) {
+          existing.data.splice(0, existing.data.length)
+          existing.data.push(...dataset.data)
+        }
+      }
+      chart.data.labels!.splice(0, chart.data.labels!.length)
+      chart.data.labels!.push(...newData.labels)
+      chart.update()
+    })
+
+    return () => {
+      chart.destroy()
+      unsub()
+    }
+  }, [])
+  return <canvas ref={canvasRef} {...props} />
+}
