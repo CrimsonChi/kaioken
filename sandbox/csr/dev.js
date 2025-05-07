71acbf346e3234274fa485aa39303946cef7b0fe
LankyMoose <rausten93@gmail.com>
2025-05-07 22:29:13 +1200

    sandbox/csr - improve dev script

--- sandbox/csr/dev.js
+++ sandbox/csr/dev.js
@@ -2,35 +2,26 @@ import { fileURLToPath } from "node:url"
 import { dirname, join } from "node:path"
 import { watch } from "node:fs"
 import { spawn } from "node:child_process"
-import { createServer } from "vite"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const pluginPath = join(
-  __dirname,
-  "../../packages/vite-plugin-kaioken/dist/index.js"
-)
-
-async function startVite() {
-  const server = await createServer()
-  await server.listen()
-  const { https, host, port } = server.config.server
-  const addr = `http${https ? "s" : ""}://${host ?? "localhost"}:${port}`
-  console.log(`Vite server running at ${addr}`)
-}
-
-// 🧠 ENTRYPOINT LOGIC
 if (process.argv.includes("--child")) {
-  startVite()
+  spawn("sh", ["-c", "vite"], { stdio: "inherit" })
 } else {
-  let child = spawn("node", [__filename, "--child"], {
-    stdio: "inherit",
-  })
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const pluginPath = join(
+    __dirname,
+    "../../packages/vite-plugin-kaioken/dist/index.js"
+  )
+
+  const createChild = () =>
+    spawn("node", [__filename, "--child"], { stdio: "inherit" })
+
+  let child = createChild()
 
   /**
-   * unsure why but our watcher is fired 3 times, so we use
-   * a simple same-tick debounce to prevent multiple restarts
+   * watcher is fired multiple times (probably esbuild being a bit aggressive),
+   * so we use a same-tick debounce to prevent multiple restarts.
+   * Could probably tack on an actual timeout duration but this seems to work well.
    */
   let restartTimeout = null
   const restart = () => {
@@ -38,7 +29,7 @@ if (process.argv.includes("--child")) {
     restartTimeout = setTimeout(() => {
       console.log("Restarting server due to plugin change...")
       child.kill()
-      child = spawn("node", [__filename, "--child"], { stdio: "inherit" })
+      child = createChild()
     })
   }
 
