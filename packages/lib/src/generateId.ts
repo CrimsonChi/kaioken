1b91b117bff267575c2337b67a37fa165975a288
LankyMoose <rausten93@gmail.com>
2025-07-12 20:41:41 +1200

    lib/generateId - move default characters to static variable

--- packages/lib/src/generateId.ts
+++ packages/lib/src/generateId.ts
@@ -1,16 +1,19 @@
+const DEFAULT_CHARACTERS =
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-" as const
+
 /**
  * Generates a random id
  * @param {number} [size=10] size of the id (in number of characters)
- * @param {string} [alphabet="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-"] set of characters to be used in the generation of the id
+ * @param {string} [characterSet=defaultCharacterSet] set of characters to be used in the generation of the id
  * @returns {string} random id of length {@link size}
  */
 export function generateRandomID(
   size: number = 10,
-  alphabet: string = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-"
+  characterSet: string = DEFAULT_CHARACTERS
 ): string {
   let id = ""
   for (let i = 0; i < size; i++) {
-    id += alphabet[(Math.random() * alphabet.length) | 0] // bitwise OR `|` is faster than `Math.floor()`
+    id += characterSet[(Math.random() * characterSet.length) | 0] // bitwise OR `|` is faster than `Math.floor()`
   }
   return id
 }
